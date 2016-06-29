4f4c18524974a0dbc064371cca229698e5242382
Erika Shiroma <eshiroma@uw.edu>
2016-06-29 14:46:05 -0700

    Support parameters to GET request

--- public/index.js
+++ public/index.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
-    $("h1").click(function() {
-    console.log("You clicked the title!");
-  });
+  render();
 });
+
+var render = function(domainId, startDate, endDate) {
+  var params = {
+    domain: domainId,
+    start: startDate,
+    end: endDate
+  };
+  $.get("/gameData", params, onData);
+};
+
+var onData = function(model) {
+  $(".data").append(JSON.stringify(model));
+  var $playerRows = {};
+  for (playerId in model.players) {
+    // TODO: replace with jade template
+    $(".playerList").append('<div class="playerRow"' + playerId + '>' + model.players[playerId].name + '</div');
+  }
+};
