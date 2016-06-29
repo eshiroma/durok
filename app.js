0bf556d36b7829334d72f59acaa9ebcea2e00928
Erika Shiroma <eshiroma@uw.edu>
2016-06-28 17:18:13 -0700

    Allow dynamic port number

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var Express = require("express");
 var Model = require("./model");
 var handler = require("./index.js");
 
-const portNumber = 3000;
-
 var app = Express();
 var model = new Model();
+var port = process.env.PORT || 3000;
 app.set("model", model);
 
 model.init(function() {
   app.get("/", handler.getIndex);
 
-  app.listen(portNumber);
-  console.log("Listening on port number ", portNumber);
+  app.listen(port);
+  console.log("Listening on port " + port);
 });
