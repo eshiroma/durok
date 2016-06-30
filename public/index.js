d16b92b77a2a56c4c99cec9848d26ec163541e45
Erika Shiroma <eshiroma@uw.edu>
2016-06-29 22:59:14 -0700

    Support date filter for table

--- public/index.js
+++ public/index.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
+  // initial page render
   render();
+
+  // TODO: submit form on "enter" keypress
 });
 
-var onFormSubmit = function(form) {
-  var domainId = form.domainSelect.options[form.domainSelect.selectedIndex].value;
-  var startDate = form.startDate.value;
-  var endDate = form.endDate.value;
-  render("notLossScore", domainId, startDate, endDate);
-}
+var model = {};
 
 var render = function(sortByStat, domainId, startDate, endDate) {
   var params = {
@@ -15,60 +13,10 @@ var render = function(sortByStat, domainId, startDate, endDate) {
     start: startDate,
     end: endDate
   };
-
-  $.get("/gameData", params, function(model) {
-    var scores = parseScores(model);
-    var tableBodyHtml = ""
-
-    var rankedPlayerIds = Object.keys(scores).sort(function(playerId, otherId) {
-      switch(sortByStat) {
-        case "name":
-          return scores[otherId].name.compareTo(scores[playerId].name);
-          break;
-        case "plays":
-          return scores[otherId].plays - scores[playerId].plays;
-          break;
-        case "notLosses":
-          return scores[otherId].notLosses - scores[playerId].notLosses;
-          break; 
-        case "losses":
-          return scores[otherId].losses - scores[playerId].losses;
-          break; 
-        case "playScore":
-          return scores[otherId].playScore - scores[playerId].playScore;
-          break;  
-        case "notLossPercent":
-          return scores[otherId].notLossPercent - scores[playerId].notLossPercent;
-          break; 
-        default: // notLossScore
-          return scores[otherId].notLossScore - scores[playerId].notLossScore;
-          break; 
-      }
-    });
-
-    rankedPlayerIds.forEach(function(playerId) {
-      tableBodyHtml += '<tr class="playerRow">'
-      + '  <td class="playerCol">' + scores[playerId].name + '</td>'
-      + '  <td class="playsCol">' + scores[playerId].plays + '</td>'
-      + '  <td class="notLossesCol">' + scores[playerId].notLosses + '</td>'
-      + '  <td class="lossesCol">' + scores[playerId].losses + '</td>'
-      + '  <td class="notLossScoreCol">' + scores[playerId].notLossScore + '</td>'
-      + '  <td class="playScoreCol">' + scores[playerId].playScore + '</td>'
-      + '  <td class="notLossPercentCol">' + scores[playerId].notLossPercent + '</td>'
-      + '</tr>';
-    });
-    $("tbody").html(tableBodyHtml);
-
-    var optionCount = 1;
-    var domainSelect = document.getElementById("domainSelect");
-    for (domainId in model.domains) {
-      domainSelect.options[optionCount] = new Option(model.domains[domainId], domainId, false, domainId == model.domainId);
-      optionCount++;
-    }
-  });
+  $.get("/gameData", params, onData);
 };
 
-var parseScores = function(model) {
+var onData = function(model) {
   var scores = {};
   for (playerId in model.players) {
     scores[playerId] = {
@@ -95,10 +43,69 @@ var parseScores = function(model) {
     });
   }
 
-  for (playerId in model.players) {
-    scores[playerId].notLosses = scores[playerId].plays - scores[playerId].losses;
-    scores[playerId].notLossPercent = 100 * scores[playerId].notLosses / scores[playerId].plays;
-  }
-
-  return scores;
+  renderTable("notLossScore", scores, model);
 }
+
+var renderTable = function(sortByStat, scores, model) {
+  var tableBodyHtml = ""
+  var rankedPlayerIds = Object.keys(scores).sort(function(playerId, otherId) {
+    switch(sortByStat) {
+      case "name":
+        return scores[otherId].name.compareTo(scores[playerId].name);
+        break;
+      case "plays":
+        return scores[otherId].plays - scores[playerId].plays;
+        break;
+      case "notLosses":
+        return scores[otherId].notLosses - scores[playerId].notLosses;
+        break; 
+      case "losses":
+        return scores[otherId].losses - scores[playerId].losses;
+        break; 
+      case "playScore":
+        return scores[otherId].playScore - scores[playerId].playScore;
+        break;  
+      case "notLossPercent":
+        return scores[otherId].notLossPercent - scores[playerId].notLossPercent;
+        break; 
+      default: // notLossScore
+        return scores[otherId].notLossScore - scores[playerId].notLossScore;
+        break; 
+    }
+  });
+
+  rankedPlayerIds.forEach(function(playerId) {
+    tableBodyHtml += '<tr class="playerRow">'
+    + '  <td class="playerCol">' + scores[playerId].name + '</td>'
+    + '  <td class="playsCol">' + scores[playerId].plays + '</td>'
+    + '  <td class="notLossesCol">' + scores[playerId].notLosses + '</td>'
+    + '  <td class="lossesCol">' + scores[playerId].losses + '</td>'
+    + '  <td class="notLossScoreCol">' + scores[playerId].notLossScore + '</td>'
+    + '  <td class="playScoreCol">' + scores[playerId].playScore + '</td>'
+    + '  <td class="notLossPercentCol">' + scores[playerId].notLossPercent + '</td>'
+    + '</tr>';
+  });
+  $("tbody").html(tableBodyHtml);
+
+  var optionCount = 1;
+  var domainSelect = document.getElementById("domainSelect");
+  for (domainId in model.domains) {
+    domainSelect.options[optionCount] = new Option(model.domains[domainId], domainId, false, domainId == model.domainId);
+    optionCount++;
+  }
+};
+
+var onFormSubmit = function(form) {
+  var domainId = form.domainSelect.options[form.domainSelect.selectedIndex].value;
+
+  var startDateSplit = form.startDate.value.split("-");
+  var endDateSplit = form.endDate.value.split("-");
+  if (startDateSplit.length >= 3) {
+    var startDate = new Date(startDateSplit[0], startDateSplit[1] - 1, startDateSplit[2]);
+  }
+  if (endDateSplit.length >= 3) {
+    var endDate = new Date(endDateSplit[0], endDateSplit[1] - 1, endDateSplit[2]);
+  }
+  
+  render("notLossScore", domainId, startDate.getTime(), endDate.getTime());
+};
