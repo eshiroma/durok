da79bf757c100074674ee49df26e2278afabf50c
Erika Shiroma <eshiroma@uw.edu>
2016-07-01 01:27:15 -0700

    Add date parsing for adding game

--- updateScores.js
+++ updateScores.js
@@ -19,11 +19,19 @@ const playerQuery = 'SELECT * FROM players WHERE players.deleted = 0';
 
 var insertDomainQuery = function(domainName) { 
   return 'INSERT INTO game_domains (name) values ("' + domainName + '")';
-}
+};
 var insertPlayerQuery = function(playerName) { 
   return 'INSERT INTO players (name) values ("' + playerName + '")';
+};
+var insertGameQuery = function(gameDate, playerCount) {
+  return 'INSERT INTO games (date, player_count) values ("' + gameDate + '",'
+    + player_count + ')';
+};
+var insertPlayerGameResultQuery = function(gameId, playerCount, playerId, isDurok) {
+  var isDurokString = isDurok ? 1 : 0;
+  return 'INSERT INTO player_game_results (game_id, player_count, player_id, is_durok) values ('
+    + gameId + ',' + player_count + ',' + player_id + ',' + isDurokString + ')';
 }
-
 
 // Prompt for password and establish db connection
 // TODO: hide password characters
@@ -192,4 +200,41 @@ var addPlayer = function(connection, rl) {
 
 var addGame = function(connection, rl) {
   console.log("add a game!");
+  rl.question("  Enter game date ('today', MM/DD, or MM/DD/YYYY): ", function(dateInput) {
+    var dateString;
+    if (dateInput === "today") {
+      dateString = sqlDateStringFromDate(new Date());
+    } else {
+      var dateInputSplit = dateInput.split("/");
+      if (dateInputSplit.length === 2 || dateInputSplit.length === 3) {
+        var month = Number(dateInputSplit[0]);
+        var day = Number(dateInputSplit[1]);
+        var year = dateInputSplit.length === 3 ? dateNumber(dateInputSplit[2]) : new Date().getFullYear();
+        if (isNan(month) || isNan(day) || isNan(year)) {
+          console.log("  Unable to add game; date contained non-numbers");
+          prompt(connection, rl);
+        } else {
+          var dateString = sqlDateStringFromDate(new Date(year, month, day));
+        }
+      } else {
+        console.log("  Unable to add game; incorrecty formatted date");
+        prompt(connection, rl);
+      }
+    }
+
+    console.log(dateString);
+  });
 }
+
+var sqlDateStringFromDate = function(date) {
+  return dateString = date.getFullYear() + "-" + zeroPad(date.getMonth() + 1, 2)
+        + "-" + zeroPad(date.getDate(), 2);
+};
+
+var zeroPad = function(x, n) {
+  var result = x.toString();
+  while (result.length < n) {
+    result = "0" + result;
+  }
+  return result;
+};
